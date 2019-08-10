4dd08bf449d6ac80c606e31fbc09b40e9f0ff3a7
omeir1 <ohad.meir@intel.com>
2019-08-10 19:28:22 +0300

    open_images_2019.py - fix after rebase

--- examples/FasterRCNN/dataset/open_images_2019.py
+++ examples/FasterRCNN/dataset/open_images_2019.py
@@ -3,53 +3,17 @@ import os
 from tensorpack.utils import logger
 
 from config import config as cfg
-from dataset import DatasetRegistry, DatasetSplit
+from dataset import DatasetRegistry
 from dataset.coco import COCODetection
 
 
 __all__ = ['register_open_images']
 
 
-# class OpenImagesDetection(DatasetSplit):
-#
-#     _IMAGES_FOLDER = {'train2019': 'train', 'val2019': 'validation'}
-#     _ANNOTATIONS_FILE = {'train2019': 'challenge-2019-train-detection-bbox.csv',
-#                          'val2019': 'challenge-2019-validation-detection-bbox.csv'}
-#
-#     def __init__(self, basedir, split):
-#         basedir = os.path.expanduser(basedir)
-#
-#         self._imgdir = os.path.realpath(os.path.join(basedir, 'images', self._IMAGES_FOLDER[split], 'original'))
-#         assert os.path.isdir(self._imgdir), "{} is not a directory!".format(self._imgdir)
-#
-#         class_desc_file = os.path.join(basedir, 'docs/challenge-2019-classes-description-500.csv')
-#         assert os.path.isfile(class_desc_file), class_desc_file
-#
-#         annotation_file = os.path.join(basedir, 'docs', self._ANNOTATIONS_FILE[split])
-#         assert os.path.isfile(annotation_file), annotation_file
-#
-#         class_desc = [l.strip().split(',') for l in open(class_desc_file)]
-#         self._class_names = [l[1] for l in class_desc]
-#         self._class_id_to_name = {l[0]: l[1] for l in class_desc}
-#         cfg.DATA.CLASS_NAMES = ["BG"] + self._class_names
-#
-#         # TODO: parse annotations file
-#         content = [l.strip().split(',') for l in open(annotation_file).readlines()[1:]]
-#         logger.info("Instances loaded from {}.".format(annotation_file))
-
-
 class OpenImagesDetection(COCODetection):
     def __init__(self, basedir, split):
         self._basedir = basedir
-
         super().__init__(basedir=basedir, split=split)
-
-        class_desc_file = os.path.join(basedir, 'docs/challenge-2019-classes-description-500.csv')
-        assert os.path.isfile(class_desc_file), class_desc_file
-
-        class_desc = [l.strip().split(',') for l in open(class_desc_file)]
-        self.class_names = [l[1] for l in class_desc]
-        cfg.DATA.CLASS_NAMES = ["BG"] + self.class_names
 
     def get_images_dir(self, split):
         return os.path.realpath(os.path.join(self._basedir, 'images', split, 'original'))
@@ -59,8 +23,20 @@ class OpenImagesDetection(COCODetection):
 
 
 def register_open_images(basedir):
-    for split in ["train", "validation"]:
-        DatasetRegistry.register("open_images_" + split, lambda x=split: OpenImagesDetection(basedir, x))
+
+    class_desc_file = os.path.join(basedir, 'docs/challenge-2019-classes-description-500.csv')
+
+    if os.path.exists(class_desc_file):
+        logger.info('Registering Open Images dataset')
+
+        class_desc = [l.strip().split(',') for l in open(class_desc_file)]
+        class_names = [l[1] for l in class_desc]
+        class_names = ["BG"] + class_names
+
+        for split in ["train", "validation"]:
+            name = "open_images_" + split
+            DatasetRegistry.register(name, lambda x=split: OpenImagesDetection(basedir, x))
+            DatasetRegistry.register_metadata(name, 'class_names', class_names)
 
 
 if __name__ == '__main__':
